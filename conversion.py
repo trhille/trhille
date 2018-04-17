422af8fa6f85d30031c13dd3c3c4a5ee0b5ae4f6
Trevor Hillebrand <trhille@uw.edu>
2018-04-17 11:15:20 -0700

    I made a Python script with some unit conversion functions in it

---
+++ conversion.py
@@ -0,0 +1,9 @@
+#This is a script that does conversions
+
+def meters2miles(meters):
+	miles = meters/1609.34
+	return miles
+
+def parsecs2inches(parsecs):
+	inches = parsecs/1.215e+18
+	return inches
